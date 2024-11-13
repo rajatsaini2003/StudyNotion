658c5899b731c19abc8f5ce580f260dde0d2c92f
rajatsaini2003 <rajatsaini9460@gmail.com>
2024-11-13 23:40:23 +0530

    Tags controller and Cloudinary upload utility function created

---
+++ Server/controllers/Course.js
@@ -0,0 +1,13 @@
+const Course = require('../models/Course');
+const Tag = require('../models/Tag');
+const User = require('../models/User');
+const {uploadImageToClodinary} = require('../utils/imageUpload');
+
+// createCourse handler function
+exports.createCourse = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
