78b27280b4b12d9a22d8f2b3dcb08454f4a47fa4
rajatsaini2003 <rajatsaini9460@gmail.com>
2024-11-15 23:58:02 +0530

    createCourse & showCourse controller created

--- Server/controllers/Course.js
+++ Server/controllers/Course.js
@@ -7,7 +7,98 @@ const {uploadImageToClodinary} = require('../utils/imageUpload');
 exports.createCourse = async (req, res) => {
     try {
         
+        // fetch data
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
+
+        //get thumbnail
+        const thumbnail = req.file.thumbnailImage;
+
+        //validation
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        // check userId
+        const userId = req.user.id;
+        const InstructorDetails =  await User.findById(userId);
+        console.log("instructor Details: ", InstructorDetails)
+        // todo: check if user and instructor id are the same or different
+        if(!InstructorDetails) return res.status(401).json({
+            success: false,
+            message: "Instructor not found"
+        });
+
+        // check given tag is valid or not
+        const tagDetails = await Tag.findOne(tag);
+        if(!tagDetails){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid tag"
+            });
+        }
+        // upload thumbnail to cloudinary
+        const thumbnailImage = await uploadImageToClodinary(thumbnail, process.env.FOLDER_NAME);
+
+        //create an entry for new course
+        const newCourse =  await Course.create({
+            courseName, 
+            courseDescription,
+            instructor:InstructorDetails._id,
+            whatYouWillLearn:whatYouWillLearn,
+            price,
+            tag:tagDetails._id,
+            thumbnail:thumbnailImage.secure_url
+        })
+        await User.findByIdAndUpdate({
+            _id:InstructorDetails._id},
+            {
+                $spush: {
+                    courses: newCourse._id
+                }
+            },
+            {new:true}
+        )
+
+        // update the tag schema
+        // TODO:HW
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "course created successfully",
+            data: newCourse
+        })
+
     } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "fail to create course",
+            error: error.message
+        })
         
+    }
+}
+
+exports.showAllCourse = async (req, res) => {
+    try {
+        // TODO change the below statement incrementally
+        const allCourse = await Course.find({}); 
+        
+        return res.status(200).json({
+            success: true,
+            allCourse,
+            message: "All courses fetched successfully"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "fail to fetch all courses",
+            error: error.message
+        })
     }
 }
