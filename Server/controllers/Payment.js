50492e0acbf0ac511f2634737352f9a935ac9a86
rajatsaini2003 <rajatsaini9460@gmail.com>
2024-11-24 22:55:47 +0530

    Razorpay signature verification controller imlemented

--- Server/controllers/Payment.js
+++ Server/controllers/Payment.js
@@ -95,4 +95,67 @@ exports.verifySignature = async (req, res) => {
 
     const signature = req.headers['x-razorpay-signature'];
     
+    const shasum = crypto.createHmac("sha256", webHookSecret);
+    shasum.update(JSON.stringify(req.body));
+    const digest = shasum.digest("hex");
+
+    if(signature === digest){
+        console.log("payment authorized") ;
+        const {courseId, userId } = req.body.payload.payment.entry.notes;
+        try {
+            const enrolledCourse  = await Course.findOneAndUpdate({_id:courseId},
+                                                {$push: {studentsEnrolled: userId}},
+                                                {new: true}
+            )
+
+            if(!enrolledCourse){
+                return res.status(500).json({
+                    success: false,
+                    message :"course not found"
+                })
+            }
+
+            console.log(enrolledCourse);
+
+            //find the student and add course to their list of enrolled courses
+            const enrolledStudent = await User.findByIdAndUpdate(userId, 
+                                                {$push: {courses: courseId}}, 
+                                                {new: true}
+            );
+            console.log(enrolledStudent);
+
+            //email send karo for confirmation about course purchase
+            const emailResponse = await mailSender(
+                enrolledStudent.email,
+                "Course Enrollment Confirmation",
+                "congraculations, you are onboarded into new NextAcademy course"
+            );
+            if(!emailResponse.success){
+                console.error("Failed to send email: " + emailResponse.message);
+                return res.status(500).json({
+                    success: false,
+                    message: "Failed to send email"
+                })
+            }
+            console.log("Email sent successfully: " + emailResponse);
+
+            return res.status(200).json({
+                success: true,
+                message: "Payment successful"
+            })
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: "Failed to verify payment" ,
+                error: error.message
+            })
+        }
+    }
+    else{
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Signature"
+        })
+    }
+
 }
