d906951493681746123228b43879e58039d8db9e
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-01-19 23:55:13 +0530

    Home Page Secton 1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <div>
+      {/* Section 1 */}
+      <div className='relative mx-auto flex flex-col w-11/12 items-center text-white 
+      justify-between  '>
+        <Link to='/signup'>
+        <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 
+        transition-all duration-200 hover:scale-95 w-fit '>
+            <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] 
+            transition-all duration-200 hover:scale-95 group-hover:bg-richblack-900'>
+                <p>Become an Instructor </p>
+                <FaArrowRight />
+            </div>
+        </div>
+        </Link>
+      </div>
+
+      {/* Section 2 */}
+
+      {/* Section 3 */}
+
+      {/* Footer */}
+
+    </div>
+  )
+}
+
+export default Home;
