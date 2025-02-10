a76ed378a8993f857f964df23e34490e5cd5f45f
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-02-10 23:27:01 +0530

    feat [UI] : created NavBar added redux toolkit reducers and slices

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import {Route,Routes} from "react-router-dom"
 import Home from './pages/Home'
+import NavBar from "./components/common/NavBar";
 function App() {
   return (
     <div className = "w-screen min-h-screen bg-richblack-900 flex flex-col font-inter ">
+      <NavBar/>
       <Routes >
         <Route path = '/' element={<Home/>} />
       </Routes>
---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link, matchPath } from 'react-router-dom'
+import logo from '../../assets/Logo/Logo-Full-Light.png'
+import { NavbarLinks } from '../../data/navbar-links'
+import { useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+const NavBar = () => {
+
+    const {token}=useSelector((state)=>state.auth);
+    const {user}=useSelector((state)=>state.profile);
+    const {totalItems} = useSelector((state)=>state.cart);
+
+
+    const location = useLocation();
+    const matchRoute =(route)=>{
+        return matchPath({path: route}, location.pathname);
+    }
+  return (
+    <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblue-700'>
+        <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
+            <Link to='/'>
+                <img src={logo} 
+                alt="StudyNotion" 
+                width={160}
+                height={42}
+                loading='lazy'/>
+            </Link>
+            {/** nav links */}
+            <nav>
+                <ul className='flex flex-row gap-x-6 text-richblack-25'>
+                    {
+                        NavbarLinks.map((link,index)=>{
+                            return (
+                                <li key={index}>
+                                    {
+                                        link?.title ==='Catalog' 
+                                        ? (<div></div>)
+                                        :(
+                                            <Link to={link?.path}>
+                                                <p className={`${matchRoute(link?.path)?"text-yellow-5":"text-richblack-25"}`}>
+                                                    {link?.title}
+                                                </p>
+                                            </Link>
+                                        )
+                                    }
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+            <div className='flex gap-x-4 items-center'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
