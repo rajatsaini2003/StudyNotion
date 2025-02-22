ad5b3e79141eb59d8372e556afd6f414bd8f4901
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-02-22 23:37:15 +0530

    applied css in aboutus page

--- src/components/core/About/Quote.jsx
+++ src/components/core/About/Quote.jsx
@@ -4,7 +4,7 @@ import HighlightText2 from '../HomePage/HighlightText2'
 
 const Quote = () => {
   return (
-    <div>
+    <div className=' text-xl md:text-4xl font-semibold mx-auto py-5 pb-20 text-center text-white'>
       We are passionate about revolutionizing the way we learn. Our innovative platform 
       <HighlightText text={" combines technology"}/>
       <HighlightText2 text={" expertise"}/>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,21 +10,22 @@ import Stats from '../components/core/About/Stats'
 import LearningGrid from '../components/core/About/LearningGrid'
 const About = () => {
   return (
-    <div className="mt-[100px] text-white ">
+    <div className=" text-white ">
         {/*Section 1 */}
-        <section>
-            <div>
-                <header >
+        <section className='bg-richblack-700'>
+            <div className='relative mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-center text-white'>
+                <header className='mx-auto py-20 text-4xl font-semibold lg:w-[70%]'>
                     Diving Innovation in Online Education for a
                     <HighlightText text={" Brighter Future"}/>
-                    <p>
+                    <p className='mx-auto mt-3 text-center text-base font-medium text-richblack-300 lg:w-[95%]'>
                         Studynotion is at the forefront of driving innovation in 
                         online education. We're passionate about creating a brighter 
                         future by offering cutting-edge courses, leveraging emerging 
                         technologies, and nurturing a vibrant learning community.
                     </p>
                 </header>
-                <div className="flex gap-x-3 mx-auto">
+                <div className="sm:h-[70px] lg:h-[150px]"></div>
+                <div className='absolute bottom-0 left-[50%] grid w-[100%] translate-x-[-50%] translate-y-[30%] grid-cols-3 gap-3 lg:gap-5'>
                     <img src ={BannerImage1} alt=""/>
                     <img src ={BannerImage2} alt=""/>
                     <img src ={BannerImage3} alt=""/>
@@ -33,22 +34,25 @@ const About = () => {
         </section>
 
         {/*Section 2 */}
-        <section>
-            <Quote/>
+        <section className='border-b border-richblack-700'>
+            <div className='mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-richblack-500'>   
+            <div className="h-[100px] "></div>
+                <Quote/>
+            </div>
         </section>
         
         {/* section 3 */}
         <section>
-            <div className="flex flex-col">
+            <div className='mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-richblack-500'>
                 {/* founding story div */}
-                <div className="flex">
+                <div className='flex flex-col items-center gap-10 lg:flex-row justify-between '>
                     {/* left div for founding story */}
-                    <div>
+                    <div className='my-24 flex lg:w-[50%] flex-col gap-10'>
                         <h1 className='bg-gradient-to-br from-[#833AB4] via-[#FD1D1D] to-[#FCB045] 
                         bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%] '>
                             Our Founding Story
                         </h1>
-                        <p>
+                        <p className='text-base font-medium text-richblack-300 lg:w-[95%]'>
                         Our e-learning platform was born out of a shared vision and passion 
                         for transforming education. It all began with a group of educators, 
                         technologists, and lifelong learners who recognized the need for 
@@ -56,7 +60,7 @@ const About = () => {
                         rapidly evolving digital world.
                         </p>
 
-                        <p>
+                        <p className='text-base font-medium text-richblack-300 lg:w-[95%]'>
                         As experienced educators ourselves, we witnessed firsthand the 
                         limitations and challenges of traditional education systems. We 
                         believed that education should not be confined to the walls of a 
@@ -67,18 +71,20 @@ const About = () => {
                     </div>
                     {/* right image div */}
                     <div>
-                        <img src={FoundingStory} alt="Founding Story"/>
+                        <img 
+                        className='shadow-[0_0_20px_0] shadow-[#FC6767]'
+                        src={FoundingStory} alt="Founding Story"/>
                     </div>
                 </div>
 
                 {/* vision and mission div */}
-                <div className="flex">
+                <div className='flex flex-col items-center lg:gap-10 lg:flex-row justify-between'>
                     {/*left box*/}
-                    <div>
-                        <h1>
+                    <div className='my-24 flex lg:w-[40%] flex-col gap-10'>
+                        <h1 className="text-4xl">
                             <HighlightText2 text={" Our Vision"}/>
                         </h1>
-                        <p>
+                        <p className='text-base font-medium text-richblack-300 lg:w-[95%]'>
                             With this vision in mind, we set out on a journey 
                             to create an e-learning platform that would 
                             revolutionize the way people learn. Our team of 
@@ -89,12 +95,12 @@ const About = () => {
                         </p>
                     </div>
                     {/**Right div */}
-                    <div>
-                        <h1>
+                    <div className='my-24 flex lg:w-[40%] flex-col gap-10'>
+                        <h1 className="text-4xl">
                             <HighlightText text={" Our Mission"}/>
                             
                         </h1>
-                        <p>
+                        <p className='text-base font-medium text-richblack-300 lg:w-[95%]'>
                             Our mission goes beyond just delivering courses online. 
                             We wanted to create a vibrant community of learners, 
                             where individuals can connect, collaborate, and learn 
